f26f68449df93669f47acf32dec06d86f7781b54
boodog785 <corybooher2@gmail.com>
2025-07-07 22:10:38 -0500

    Initial commit

---
+++ frontend/my-crypto-app/src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import MyToken from './contracts/MyToken.json';
+
+function App() {
+  const [account, setAccount] = useState('');
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const loadBlockchainData = async () => {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = MyToken.networks[networkId];
+      const instance = new web3.eth.Contract(MyToken.abi, deployedNetwork && deployedNetwork.address);
+      setToken(instance);
+    };
+
+    loadBlockchainData();
+  }, []);
+
+  return (
+    <div>
+      <h1>My Crypto Platform</h1>
+      <p>Your Account: {account}</p>
+    </div>
+  );
+}
+
+export default App;
